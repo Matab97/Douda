0432987884a6de6dc984da64815b599788b90036
Matab97 <medabbad0@gmail.com>
2018-11-28 09:28:44 +0100

    ADDING CHOOSE SCREEN

---
+++ app/src/main/java/com/example/abbad/douda/vsAiActivity.java
@@ -0,0 +1,13 @@
+package com.example.abbad.douda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vsAiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vs_ai);
+    }
+}
